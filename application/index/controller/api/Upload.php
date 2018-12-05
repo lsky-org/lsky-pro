162b92597d5f69498811eb25f49fbf8b4f7cf8ab
wispx <1591788658@qq.com>
2018-12-05 18:01:53 +0800

    up api

--- application/index/controller/api/Upload.php
+++ application/index/controller/api/Upload.php
@@ -10,5 +10,19 @@ namespace app\index\controller\api;
 
 class Upload extends Base
 {
+    public function initialize($auth = false)
+    {
+        parent::initialize($auth);
+        // 是否允许游客上传
+        if ($this->config['allowed_tourist_upload']) {
+            $this->token && $this->auth($this->token);
+        } else {
+            $this->auth($this->token);
+        }
+    }
 
+    public function index()
+    {
+
+    }
 }
