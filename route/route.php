2a3933b436ca20daad2f965471b69ccfb64f3447
Wisp X <1591788658@qq.com>
2019-08-04 15:22:35 +0800

    :bug: 修复接口上传header增加token后请求失败的bug

--- route/route.php
+++ route/route.php
@@ -17,6 +17,8 @@ Route::view('compatibility', 'index@tpl/compatibility');
 Route::group('api', function () {
     Route::post('token', 'api/Token/index');
     Route::post('upload', 'api/Upload/index');
-})->allowCrossDomain();
+})
+    ->header('Access-Control-Allow-Headers', 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With, Token')
+    ->allowCrossDomain();
 
 return [];
