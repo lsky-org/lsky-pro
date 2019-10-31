801a852768a2f207ab759ebb91e292406931dea9
wisp-x <1591788658@qq.com>
2019-10-31 11:59:19 +0800

    :art: 改进图片接口

--- application/api/controller/Image.php
+++ application/api/controller/Image.php
@@ -19,14 +19,14 @@ class Image extends Base
     {
         parent::initialize();
         $this->model = new Images();
-        $this->model = $this->model->where('user_id', $this->user->id);
+        $this->model = $this->model->where('user_id', $this->user->id)->field(['user_id', 'folder_id'], true);
     }
 
     public function find()
     {
         $id = $this->param('id');
         $image = $this->model->where(['id' => $id])->find();
-        $this->response('success', $image);
+        $this->response('success', $this->parseData($image));
     }
 
     public function items()
@@ -36,7 +36,18 @@ class Image extends Base
         $images = $this->model->paginate(null, false, [
             'page' => $page,
             'list_rows' => $rows,
-        ]);
+        ])->each(function ($item) {
+            $item = $this->parseData($item);
+            unset($item['create_time']);
+            return $item;
+        });
         $this->response('success', $images);
+    }
+
+    private function parseData($data)
+    {
+        $data['upload_time'] = $data->getData('create_time');
+        $data['upload_date'] = $data->create_time;
+        return $data;
     }
 }
