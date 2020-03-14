5a41b1dc1ed90253dbe8c7ec51398fe9ddb8287a
WispX <1591788658@qq.com>
2020-03-14 17:45:22 +0800

    :sparkles: 完成平滑升级功能

--- application/index/controller/admin/System.php
+++ application/index/controller/admin/System.php
@@ -8,7 +8,6 @@
 
 namespace app\index\controller\admin;
 
-use app\common\model\Config;
 use think\Db;
 use app\common\model\Images;
 use think\Exception;
@@ -90,41 +89,84 @@ class System extends Base
     {
         Db::startTrans();
         try {
-            $upgrade = new \Upgrade(app()->getRootPath(), 'v1.5.8');
-
+            $upgrade = new \Upgrade(app()->getRootPath(), 'v' . $this->config['system_version']);
             $releases = $upgrade->releases(); // 获取安装包列表
             $release = current($releases); // 获取最新版
-
             // 判断是否已经是最新版
             if ($upgrade->check($release->version)) {
                 throw new \Exception('当前系统已经是最新版');
             }
-
-            // 备份 sql
-            $this->backupSql($upgrade->getPath() . 'backup.sql');
-
             $upgrade->backup('backup-' . date('YmdHis') . '.zip'); // 备份系统
+            $upgradeFile = app()->getRuntimePath() . 'upgrade.zip';// 判断是否存在安装包
+            $file = file_exists($upgradeFile) ? $upgradeFile : $upgrade->download($release->url);
 
-            $file = $upgrade->download($release->url); // 下载安装包
-
-            // 校验 MD5, 校验失败则删除文件并抛出异常
+            // 校验 MD5
             if (md5_file($file) !== $release->md5) {
                 throw new \Exception('安装包损坏, 请稍后重试');
             }
 
             $dir = $upgrade->unzip($file, $upgrade->getWorkspace()); // 解压安装包到工作区目录
-            // TODO 根据版本包配置, 导入 sql
-            // TODO sql 导入成功后根据配置删除指定文件, 然后再覆盖文件
+            $path = rtrim($dir . $release->path, '/') . '/'; // 新版本程序根目录
+            $sqlPath = $path . $release->sql; // sql 文件路径
+            if (!$sql = @file_get_contents($sqlPath)) {
+                throw new \Exception('SQL 文件获取失败');
+            }
+
+            // 检测新增表字段
+            if (!$tableFields = @include($path . $release->files['table_fields'])) {
+                throw new \Exception('表字段配置文件获取失败');
+            }
+            foreach ($tableFields as $table => $fields) {
+                foreach ($fields as $field => $sql) {
+                    $fetchFields = array_column(Db::query("DESCRIBE `{$table}`;"), 'Field');
+                    if (!in_array($field, $fetchFields)) {
+                        Db::execute($sql);
+                    }
+                }
+            }
+
+            // 执行 sql 导入
+            if (is_file($path . $release->sql)) {
+                $lines = file($path . $release->sql);
+                $temp = '';
+                foreach ($lines as &$line) {
+                    $line = trim($line);
+                    if (substr($line, 0, 2) == '--' || $line == '' || substr($line, 0, 2) == '/*') continue;
+                    $temp .= $line;
+                    if (substr($line, -1, 1) == ';') {
+                        Db::execute($temp);
+                        $temp = '';
+                    }
+                }
+            }
+
+            // 检测需要的删除文件或文件夹
+            foreach ($release->removes as $key => $items) {
+                foreach ($items as $item) {
+                    $pathname = trim($upgrade->getRootPath(), '/') . '/' . trim($item, '/');
+                    if ($key === 'folders') {
+                        $upgrade->rmdir($pathname);
+                    } else {
+                        @unlink($pathname);
+                    }
+                }
+            }
+
+            // 删除安装包 sql 文件
+            @unlink($sqlPath);
+            @unlink($path . 'install.sql');
+
+            // 复制文件夹
+            $upgrade->copyDirs($path, $upgrade->getRootPath());
+
             Db::commit();
         } catch (\Exception $e) {
             Db::rollback();
-            $this->error($e->getMessage());
+            $this->result([], 0, $e->getMessage());
+        } catch (\PDOException $e) {
+            Db::rollback();
+            $this->result([], 0, $e->getMessage());
         }
-        $this->success('更新完成');
-    }
-
-    private function backupSql($pathname)
-    {
-
+        $this->result([], 1, '更新完成');
     }
 }
--- extend/Upgrade.php
+++ extend/Upgrade.php
@@ -15,7 +15,7 @@ class Upgrade
      *
      * @var
      */
-    private $path;
+    private $rootPath;
 
     /**
      * 工作区目录
@@ -44,13 +44,15 @@ class Upgrade
      */
     public function __construct($path, $version)
     {
-        $this->path = rtrim(str_replace('\\', '/', $path), '/') . '/';
-        $this->workspace = $this->path . 'runtime/.tmp/';
+        $this->rootPath = rtrim(str_replace('\\', '/', $path), '/') . '/';
+        $this->workspace = $this->rootPath . 'runtime/.tmp/';
         $this->version = $version;
 
         if (!class_exists('ZipArchive')) {
             throw new \Exception('无法继续执行, 请确保 ZipArchive 正确安装');
         }
+
+        ob_clean();
     }
 
     /**
@@ -66,31 +68,40 @@ class Upgrade
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '256M');
 
+        $ip = $this->getRandIp();
         $headers = [
-            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+            'Accept-Encoding: gzip, deflate',
+            'User-Agent: ' . $this->getRandUserAgent(),
+            'X-FORWARDED-FOR: ' . $ip,
+            'CLIENT-IP: ' . $ip,
         ];
+
+        !is_dir($this->workspace) && @mkdir($this->workspace, 0777, true);
+        if (!is_dir($this->workspace)) throw new \Exception('工作区目录不存在, 请检查是否有写入权限');
+        $pathname = $this->workspace . 'upgrade.zip';
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_ENCODING, 'gzip, delete');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 180);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 180);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         $contents = curl_exec($curl);
         $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        /*$headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $header = substr($contents, 0, $headerSize);*/
+
         curl_close($curl);
 
         if ($statusCode !== 200) {
             throw new \Exception('增量包下载失败, 请稍后重试!');
         }
 
-        $this->rmdir($this->workspace);
-        @mkdir($this->workspace, 0777, true); // 创建工作目录
-
-        $pathname = $this->workspace . 'upgrade.zip';
         if (!@file_put_contents($pathname, $contents)) {
             throw new \Exception('安装包保存失败, 请检查 runtime 目录是否有写入权限');
         }
@@ -108,9 +119,6 @@ class Upgrade
      */
     public function unzip($file, $dir)
     {
-        $this->rmdir($dir);
-        @mkdir($dir, 0777, true);
-
         $zip = new \ZipArchive;
         if ($zip->open($file) !== true) {
             throw new \Exception('无法打开安装包文件');
@@ -176,7 +184,7 @@ class Upgrade
      * @param $dir
      * @return bool
      */
-    private function rmdir($dir)
+    public function rmdir($dir)
     {
         if (!$handle = @opendir($dir)) return false;
 
@@ -202,17 +210,17 @@ class Upgrade
      */
     public function backup($pathname)
     {
-        $pathname = $this->path . $pathname;
+        $pathname = $this->rootPath . $pathname;
         $zip = new \ZipArchive;
         $zip->open($pathname, \ZipArchive::CREATE);
         $files = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($this->path,
+            new RecursiveDirectoryIterator($this->rootPath,
                 RecursiveDirectoryIterator::SKIP_DOTS),
             RecursiveIteratorIterator::CHILD_FIRST
         );
         foreach ($files as $file) {
             $filePath = $file->getPathName();
-            $localName = str_replace($this->path, '', $filePath);
+            $localName = str_replace($this->rootPath, '', $filePath);
             if ($file->isFile()) {
                 $zip->addFile($filePath, $localName);
             } elseif ($file->isDir()) {
@@ -224,11 +232,100 @@ class Upgrade
     }
 
     /**
+     * 复制文件夹
+     * @param string $source 源文件夹
+     * @param string $dest 目标文件夹
+     */
+    public function copyDirs($source, $dest)
+    {
+        if (!is_dir($dest)) {
+            mkdir($dest, 0755, true);
+        }
+        foreach (
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            ) as $item
+        ) {
+            if ($item->isDir()) {
+                $sontDir = $dest . '/' . $iterator->getSubPathName();
+                if (!is_dir($sontDir)) {
+                    mkdir($sontDir, 0755, true);
+                }
+            } else {
+                copy($item, $dest . '/' . $iterator->getSubPathName());
+            }
+        }
+    }
+
+    /**
+     *  获取随机 UserAgent
+     *
      * @return mixed
      */
-    public function getPath()
+    private function getRandUserAgent()
     {
-        return $this->path;
+        $array = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Macintosh; U; Mac OS X Mach-O; en-US; rv:2.0a) Gecko/20040614 Firefox/3.0.0 ",
+            "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.0.3) Gecko/2008092414 Firefox/3.0.3",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.1) Gecko/20090624 Firefox/3.5",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.14) Gecko/20110218 AlexaToolbar/alxf-2.0 Firefox/3.6.14",
+            "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+            "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+            "Opera/9.80 (Android 2.3.4; Linux; Opera mobi/adr-1107051709; U; zh-cn) Presto/2.8.149 Version/11.10",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/533.17.8 (KHTML, like Gecko) Version/5.0.1 Safari/533.17.8",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0",
+            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)"
+        ];
+
+        return $array[array_rand($array)];
+    }
+
+    /**
+     * 随机IP
+     *
+     * @return string
+     */
+    private function getRandIp()
+    {
+        $array = ["218", "218", "66", "66", "218", "218", "60", "60", "202", "204", "66", "66", "66", "59", "61", "60", "222", "221", "66", "59", "60", "60", "66", "218", "218", "62", "63", "64", "66", "66", "122", "211"];
+        $rand = mt_rand(0, count($array));
+        $ip1id = $array[$rand];
+        $ip2id = round(rand(600000, 2550000) / 10000);
+        $ip3id = round(rand(600000, 2550000) / 10000);
+        $ip4id = round(rand(600000, 2550000) / 10000);
+        return $ip1id . "." . $ip2id . "." . $ip3id . "." . $ip4id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRootPath()
+    {
+        return $this->rootPath;
     }
 
     /**
@@ -249,6 +346,6 @@ class Upgrade
 
     public function __destruct()
     {
-        is_dir($this->workspace) && $this->rmdir($this->workspace); // 清除临时工作目录
+        $this->rmdir($this->workspace); // 清除临时工作目录
     }
 }
