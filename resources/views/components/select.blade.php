380459482996dcdad7a63ce18f40cdb64d6e778a
Wisp X <wisp-x@qq.com>
2022-01-25 08:38:28 +0800

    :bug: 修复 BUG

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
-<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0']) !!}>
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
     {{ $slot }}
 </select>
