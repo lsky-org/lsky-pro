90239f9827cf919387cf77160bb0d1372e54a333
Wisp X <wisp-x@qq.com>
2022-02-12 22:07:19 +0800

    :bug: 修复 BUG

--- resources/views/layouts/user-nav.blade.php
+++ resources/views/layouts/user-nav.blade.php
@@ -14,7 +14,7 @@
             @csrf
             <x-dropdown-link href="{{ route('upload') }}">上传图片</x-dropdown-link>
             <x-dropdown-link href="{{ route('dashboard') }}">仪表盘</x-dropdown-link>
-            <x-dropdown-link href="{{ route('/') }}">设置</x-dropdown-link>
+            <x-dropdown-link href="{{ route('settings') }}">设置</x-dropdown-link>
             <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                 {{ __('Log Out') }}
             </x-dropdown-link>
