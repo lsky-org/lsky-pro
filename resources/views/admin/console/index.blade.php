28dc5b0e2ec607ce9407527d17aaca4a98b08ff8
Wisp X <wisp-x@qq.com>
2022-02-17 16:45:58 +0800

    :lipstick: Updating the UI and style files.

--- resources/views/admin/console/index.blade.php
+++ resources/views/admin/console/index.blade.php
@@ -103,7 +103,7 @@
                         {{ phpversion() }}
                     </dd>
                 </div>
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">文件上传限制</dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                         {{ ini_get("upload_max_filesize") }}
