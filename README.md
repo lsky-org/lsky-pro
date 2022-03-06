f7982d89e85cf9fe55e8477c40bdc4e6e444d0bc
Wisp X <wisp-x@qq.com>
2022-03-06 15:24:32 +0800

    :sparkles: 增加命名规则对照表

--- README.md
+++ README.md
@@ -24,17 +24,19 @@
 
 ### 📌 TODO
 * [ ] 支持多种第三方云储存，本地、AWS S3、阿里云 OSS、腾讯云 COS、七牛云、又拍云、SFTP、FTP、WebDav、Minio
-* [x] 多图上传、拖拽上传、粘贴上传、复制、一键复制链接、页面响应式布局
-* [x] 强大的图片管理功能，支持鼠标右键、单选多选、重命名等操作
+* [x] 多种数据库驱动支持，`MySQL 5.7+`、`PostgreSQL 9.6+`、`SQLite 3.8.8+`、`SQL Server 2017+`
+* [x] 支持配置使用多种缓存驱动，`Memcached`、`Redis`、`DynamoDB`、等其他关系型数据库，默认以文件的方式缓存
+* [x] 多图上传、拖拽上传、粘贴上传、动态设置策略上传、复制、一键复制链接
+* [x] 强大的图片管理功能，瀑布流展示，支持鼠标右键、单选多选、重命名等操作
 * [x] 自由度极高的角色组配置，可以为每个组配置多个储存策略，同时储存策略可以配置多个角色组
 * [x] 可针对角色组设置上传文件、文件夹路径命名规则、上传频率限制、图片审核等功能
-* [x] 支持图片水印、文字水印功能
-* [x] 支持通过接口上传、管理图片
-* [x] 支持在线增量更新
-* [x] 图片广场(画廊)
+* [x] 支持图片水印、文字水印、水印平铺、设置水印位置、X/y 轴偏移量设置、旋转角度等
+* [x] 支持通过接口上传、管理图片、管理相册
+* [x] 支持在线增量更新、跨版本更新
+* [x] 图片广场
 
 ### 💰 捐赠
-Lsky Pro 的开发和更新等，都是作者在空余时间独立开发，并免费开源使用，如果您认可我的作品，并且觉得对你有所帮助我愿意接受来自各方面的捐赠😃。
+Lsky Pro 的开发和更新等，都是作者在业余时间独立开发，并免费开源使用，如果您认可我的作品，并且觉得对你有所帮助我愿意接受来自各方面的捐赠😃。
 <table width="100%">
     <tr>
         <th>支付宝</th>
@@ -48,9 +50,11 @@ Lsky Pro 的开发和更新等，都是作者在空余时间独立开发，并�
 
 ### 😋 鸣谢
 - [Laravel](https://laravel.com)
+- [intervention/image](https://github.com/Intervention/image)
 - [overtrue](https://github.com/overtrue)
 - [league](https://github.com/thephpleague)
 - [Jquery](https://jquery.com)
+- [jQuery-File-Upload](https://github.com/blueimp/jQuery-File-Upload)
 - [Alpinejs](https://alpinejs.dev/)
 - [Viewer.js](https://github.com/fengyuanchen/viewerjs)
 - [DragSelect](https://github.com/ThibaultJanBeyer/DragSelect)
---
+++ resources/views/admin/group/rules.blade.php
@@ -0,0 +1,74 @@
+<x-modal>
+    <div id="modal-content">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+            <tr>
+                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">
+                    规则
+                </th>
+                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">
+                    说明
+                </th>
+            </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{Y}</td>
+                <td class="px-3 py-2 text-sm">年份(2022)</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{y}</td>
+                <td class="px-3 py-2 text-sm">两位数年份(22)</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{m}</td>
+                <td class="px-3 py-2 text-sm">月份(01)</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{d}</td>
+                <td class="px-3 py-2 text-sm">当月的第几号(04)</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{timestamp}</td>
+                <td class="px-3 py-2 text-sm">时间戳(秒)</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{uniqid}</td>
+                <td class="px-3 py-2 text-sm">唯一字符串</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{md5}</td>
+                <td class="px-3 py-2 text-sm">随机 md5 值</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{md5-16}</td>
+                <td class="px-3 py-2 text-sm">16位随机 md5 值</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{str-random-16}</td>
+                <td class="px-3 py-2 text-sm">16位随机字符串</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{str-random-10}</td>
+                <td class="px-3 py-2 text-sm">10位随机字符串</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{filename}</td>
+                <td class="px-3 py-2 text-sm">文件原始名称</td>
+            </tr>
+            <tr>
+                <td class="px-3 py-2 text-gray-600 text-sm">{uid}</td>
+                <td class="px-3 py-2 text-sm">用户 ID，游客为 0</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</x-modal>
+
+@push('scripts')
+    <script>
+        $('#rename-rules').click(function () {
+            Alpine.store('modal').open = true;
+        })
+    </script>
+@endpush
