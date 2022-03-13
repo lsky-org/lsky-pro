7d5df4cfbac8bfb26984320d05512095bca7b71e
Wisp X <1591788658@qq.com>
2022-03-13 19:58:54 +0800

    remove polyfilljs

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,6 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="https://polyfill.io/v3/polyfill.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
