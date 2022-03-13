4ba44d7a1654e77997d20ee7fffe71e4956273f9
Wisp X <1591788658@qq.com>
2022-03-13 16:08:32 +0800

    add polyfill

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
+    <script src="https://polyfill.io/v3/polyfill.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body class="font-sans antialiased overflow-hidden">
