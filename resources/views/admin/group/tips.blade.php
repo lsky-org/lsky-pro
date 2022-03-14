0c8e4f20c3eea38db8e3ab3be71b5a49cb9e7bb1
Wisp X <1591788658@qq.com>
2022-03-14 10:47:04 +0800

    角色组增加 tips

--- resources/views/admin/group/tips.blade.php
+++ resources/views/admin/group/tips.blade.php
@@ -6,3 +6,6 @@
 <p class="bg-yellow-500 p-2 mb-2 rounded text-sm text-white">
     <i class="fas fa-exclamation-circle"></i> 系统运行环境允许上传大小的最大值为 {{ ini_get('upload_max_filesize') }}，最大 POST 数据大小为 {{ ini_get('post_max_size') }}，上传文件大小不得超过这两项配置值。
 </p>
+<p class="bg-yellow-500 p-2 mb-2 rounded text-sm text-white">
+    <i class="fas fa-exclamation-circle"></i> 原图保护以及水印功能，原理是使用 PHP 接管图片请求，动态处理后缓存之后通过载入缓存到内存中输出图片，对服务器有着较高的要求，请谨慎使用。如果你使用第三方储存，兰空图床更推荐你使用第三方储存的图片处理规则。
+</p>
