3b1d5669737f6224ae2b68e70ffb9d8be16ba902
Wisp X <1591788658@qq.com>
2022-03-18 09:14:17 +0800

    水印合成跳过 gif、ico 格式图片

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -154,8 +154,8 @@ class Controller extends BaseController
                 $contents = Cache::get($cacheKey);
             } else {
                 $contents = $image->filesystem()->read($image->pathname);
-                // 是否启用了水印功能，跳过gif图片
-                if ($image->group?->configs->get(GroupConfigKey::IsEnableWatermark) && $image->mimetype !== 'image/gif') {
+                // 是否启用了水印功能，跳过gif和ico图片
+                if ($image->group?->configs->get(GroupConfigKey::IsEnableWatermark) && ! in_array($image->extension, ['ico', 'gif'])) {
                     $configs = $image->group?->configs->get(GroupConfigKey::WatermarkConfigs);
                     $contents = $service->stickWatermark($contents, collect($configs))->encode()->getEncoded();
                 }
