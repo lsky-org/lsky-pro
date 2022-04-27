6291acbcee8da56c76dc10cc84f80f41f02df488
Wisp X <1591788658@qq.com>
2022-04-27 15:51:53 +0800

    给图片表增加索引

---
+++ database/migrations/2022_04_27_153631_update_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->index('key');
+            $table->index('md5');
+            $table->index('sha1');
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropIndex('key');
+            $table->dropIndex('md5');
+            $table->dropIndex('sha1');
+            $table->dropIndex('created_at');
+        });
+    }
+};
