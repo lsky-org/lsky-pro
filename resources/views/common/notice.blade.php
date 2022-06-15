2086daf1ae4855d554358188bab7f5a43c5225d1
Wisp X <1591788658@qq.com>
2022-06-15 10:17:55 +0800

    改进公告弹窗 Closed #447

--- resources/views/common/notice.blade.php
+++ resources/views/common/notice.blade.php
@@ -1,15 +1,22 @@
-@if(\App\Utils::config(\App\Enums\ConfigKey::SiteNotice))
+@if(strip_tags(\App\Utils::config(\App\Enums\ConfigKey::SiteNotice)))
+
     <x-modal id="notice-modal">
         <div class="markdown-body">
             {!! (new Parsedown())->parse(\App\Utils::config(\App\Enums\ConfigKey::SiteNotice)) !!}
+        </div>
+        <div class="mt-4 w-full text-right">
+            <x-button type="button" @click="$store.modal.close('notice-modal');">OK</x-button>
         </div>
     </x-modal>
 
     @push('scripts')
         <script>
-            if (! sessionStorage.getItem('noticed')) {
-                Alpine.store('modal').open('notice-modal')
-                sessionStorage.setItem('noticed', '1');
+            let noticeHash = "{{ md5(\App\Utils::config(\App\Enums\ConfigKey::SiteNotice)) }}";
+            if (localStorage.getItem('notice-hash') !== noticeHash) {
+                setTimeout(function () {
+                    Alpine.store('modal').open('notice-modal');
+                    localStorage.setItem('notice-hash', noticeHash);
+                }, 1000)
             }
         </script>
     @endpush
