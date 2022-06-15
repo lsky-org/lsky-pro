8f6da67f4fa6fe2cc26aae220b551c98f9df888c
Wisp X <1591788658@qq.com>
2022-06-15 10:30:53 +0800

    改进 dropdown 组件

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="transition-all duration-300 w-full h-14 bg-gray-700 text-white flex justify-center fixed top-0 z-[9] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.5)]">
+<header class="transition-all duration-300 w-full h-14 bg-gray-700 text-white flex justify-center fixed top-0 z-[9] shadow-[0_15px_10px_-15px_rgba(0,0,0,0.3)]">
     <x-container class="w-full px-6 flex justify-between items-center">
         <div class="flex justify-start items-center max-w-[70%]">
             <a href="javascript:void(0)" @click="$store.sidebar.toggle()" class="w-6 h-6 p-4 rounded-full sm:hidden -ml-1 mr-4 flex justify-center items-center">
