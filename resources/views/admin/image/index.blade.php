035edd47eafc9f0fc0bafa93613ec4d8c02634b8
TeacherDu <46226528+penndu@users.noreply.github.com>
2022-07-11 00:18:54 +0800

    fix a bug
    https://github.com/lsky-org/lsky-pro/issues/473

--- resources/views/admin/image/index.blade.php
+++ resources/views/admin/image/index.blade.php
@@ -194,7 +194,7 @@
             </dl>
             <dl>
                 <div class="bg-gray-50 px-2 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-500">剩余容量</dt>
+                    <dt class="text-sm font-medium text-gray-500">已用容量</dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 truncate">__surplus_capacity__</dd>
                 </div>
             </dl>
