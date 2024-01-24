820f1ac4a64ad2f3842b098d22f26f0da496cbe1
zqu1016@qq.com <zqu1016@qq.com>
2024-01-24 16:48:50 +0800

    fixed:修复安装报错“Specified key was too long; max key length is 1000 bytes”

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            // $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
